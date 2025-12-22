10921cb25d3ef313ab49cf6050abad5fc015852b
Nikolyamba <niko7tkc@yandex.ru>
2025-12-22 08:51:40 +0300

    страдаю с html css и js

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,10 @@ import uvicorn
 from fastapi import FastAPI
 
 from backend.api.user_api import u_router
-from frontend.main import start_ui
 
 app = FastAPI()
 
 app.include_router(u_router, prefix='/api')
-
-start_ui(app)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level='info', reload=True)
---
+++ frontend/scripts/registration.js
@@ -0,0 +1,2 @@
+function postRegister(url = '127.0.0.1:8000/register'){
+}
